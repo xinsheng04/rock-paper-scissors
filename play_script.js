34fd02530fb0e6765e67340db3d49b83863a9df9
xinsheng04 <freddy161204@gmail.com>
2024-08-19 17:52:52 +0800

    Done first version

--- play_script.js
+++ play_script.js
@@ -1,75 +1,116 @@
 function getComputerChoice(){
     let choice = Math.floor(Math.random()*3);
-    return choice;
-}
-
-function getHumanChoice(){
-    let choice = prompt("Enter rock, paper or scissors: ");
-    choice = choice.toUpperCase();
-    while(choice != "ROCK" && choice != "PAPER" && choice != "SCISSORS"){
-        prompt("Invalid choice: Please enter either rock, paper or scissors.");
-        choice = prompt("Enter rock, paper or scissors: ");
-        choice = choice.toUpperCase();
-    }
     switch (choice){
-        case "ROCK":
-            return 0;
-        case "PAPER":
-            return 1;
-        case "SCISSORS":
-            return 2;
+        case 0:
+            computerChoice = "rock";
+            computerIcon.src = "./icons/rock.png";
+            break;
+        case 1:
+            computerChoice = "paper";
+            computerIcon.src = "./icons/paper.png";
+            break;
+        case 2:
+            computerChoice = "scissors";
+            computerIcon.src = "./icons/scissors.png";
+            break;
     }
 }
 
-function playRound(humanChoice, computerChoice){
-    if (humanChoice == 2 && computerChoice == 0){
-        computerScore++;
-        console.log("You lose! Rock beats Scissors! ");
+function playRound(){
+    let temp;
+    if (humanChoice == "scissors" && computerChoice == "rock"){
+        temp = ++computerScore.textContent;
+        computerScore.textContent = temp;
+        result.textContent = "You lose! Rock beats Scissors! "
     }
-    else if (humanChoice == 0 && computerChoice == 2){
-        humanScore++;
-        console.log("You win! Rock beats Scissors!");
+    else if (humanChoice == "rock" && computerChoice == "scissors"){
+        temp = ++humanScore.textContent;
+        humanScore.textContent = temp;
+        result.textContent = "You win! Rock beats Scissors!";
     }
-    else if (humanChoice == 0 && computerChoice ==1){
-        computerScore++;
-        console.log("You lose! Paper beats Rock!");
+    else if (humanChoice == "rock" && computerChoice =="paper"){
+        temp = ++computerScore.textContent;
+        computerScore.textContent = temp;
+        result.textContent = "You lose! Paper beats Rock!";
     }
-    else if (humanChoice == 1 && computerChoice ==0){
-        humanScore++;
-        console.log("You win! Paper beats Rock!");
+    else if (humanChoice == "paper" && computerChoice =="rock"){
+        temp = ++humanScore.textContent;
+        humanScore.textContent = temp;
+        result.textContent = "You win! Paper beats Rock!";
     }
-    else if (humanChoice ==1 && computerChoice == 2){
-        computerScore++;
-        console.log("You lose! Scissors beats Paper!");
+    else if (humanChoice =="paper" && computerChoice == "scissors"){
+        temp = ++computerScore.textContent;
+        computerScore.textContent = temp;
+        result.textContent = "You lose! Scissors beats Paper!";
     }
-    else if(humanChoice ==2 && computerChoice == 1){
-        humanScore++;
-        console.log("You win! Scissors beats Paper!");
+    else if(humanChoice =="scissors" && computerChoice == "paper"){
+        temp = ++humanScore.textContent;
+        humanScore.textContent = temp;
+        result.textContent = "You win! Scissors beats Paper!";
     }
     else{
-        humanScore+=0.5;
-        computerScore+=0.5;
+        result.textContent = "Draw!";
     }
 }
 
-function playGame(rounds){
-    for(let count=0;count<rounds;count++){
-        let comp = getComputerChoice();
-        let human = getHumanChoice();
-        playRound(comp, human);
+function handleChoiceSelection(button) {
+    if (previousButton) {
+      previousButton.style.backgroundColor='gray';
     }
-    console.log("End of the game!");
-    console.log("Human score: " + humanScore);
-    console.log("Computer score: " + computerScore);
-    if(computerScore>humanScore)
-        console.log("The winner is: " + "Computer");
-    else if(computerScore<humanScore)
-        console.log("The winner is: " + "Human");
-    else
-        console.log("The game is a tie!");
-    
+    button.style.backgroundColor='green';
+    humanChoice = button.id;
+    if(button.id == "rock")
+        humanIcon.src = './icons/rock.png';
+    else if(button.id == "paper")
+        humanIcon.src = './icons/paper.png';
+    else if(button.id == "scissors")
+        humanIcon.src = './icons/scissors.png';
+    previousButton = button;
+}
+
+function detectWinCondition(){
+    if(humanScore.textContent < winScore && computerScore.textContent < winScore)
+        return;
+    else if(humanScore.textContent >= 5){
+        result.textContent = "Player Victory!";
+        result.style.backgroundColor = "green";
+    }
+    else if(computerScore.textContent >= 5){
+        result.textContent = "Computer Victory!";
+        result.style.backgroundColor = "red";
+    }
+    stopGame = true;
 }
 
 //execution starts here
-let humanScore = 0, computerScore = 0;
-playGame(5);
+//initialize variables
+const humanScore = document.getElementById("playerscore");
+const computerScore = document.getElementById("computerscore");
+const humanIcon = document.getElementById("human-choice");
+const computerIcon = document.getElementById("computer-choice");
+const result = document.getElementById("result");
+let humanChoice = null, computerChoice = null;
+
+let stopGame = false;//flag to stop the game when victory is obtained
+let winScore = 5; //win condition
+//initialize scores
+humanScore.textContent = 0;
+computerScore.textContent = 0;
+
+//add logic to choice selectors
+const buttons = document.querySelectorAll(".panel button");
+let previousButton = null;
+for(let i = 0; i<buttons.length-1; i++){
+    buttons[i].addEventListener("click", () => handleChoiceSelection(buttons[i]));
+}
+//add logic to confirmation button
+buttons[buttons.length-1].addEventListener("click", () =>{
+    if(stopGame) return;
+    else if(humanChoice==null)
+        return;
+    else{
+        getComputerChoice();
+        playRound();
+        detectWinCondition();
+    }
+});
